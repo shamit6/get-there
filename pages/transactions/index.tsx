92d4ec653f2edfa64ba495ee34cc437345572b8c
Dean Shub <deanshub@gmail.com>
2021-07-17 12:51:12 +0300

    linting with eslint and prettier (#1)
    commit 4907f7fbbd7f4ba697ff3b39b80f781ac7140d3d
    Author: Dean Shub <deanshub@gmail.com>
    Date:   Fri Jul 16 20:31:05 2021 +0300
    
        removing redundent file
    
    commit 0780bf777ee594a4babca2a29d9a197340250f88
    Author: Dean Shub <deanshub@gmail.com>
    Date:   Fri Jul 16 20:25:08 2021 +0300
    
        linting with eslint and prettier

--- pages/transactions/index.tsx
+++ pages/transactions/index.tsx
@@ -1,28 +1,35 @@
-import { format } from 'date-fns';
+import { format } from 'date-fns'
 import Link from 'next/link'
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { createOrUpdateBalanceStatus, getAllTransactions, getBalanceStatus } from '../../utils/db';
-import { getCurrentBalanceAmount } from '../../utils/transactionsCalculator';
-import { TransactionConfig } from '../../utils/types';
-import styles from './Transactions.module.scss';
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import {
+  createOrUpdateBalanceStatus,
+  getAllTransactions,
+  getBalanceStatus,
+} from '../../utils/db'
+import { getCurrentBalanceAmount } from '../../utils/transactionsCalculator'
+import { TransactionConfig } from '../../utils/types'
+import styles from './Transactions.module.scss'
 
 function List() {
-  const [transactions, setTransactions] = useState<TransactionConfig[]>([]);
-  const [balance, setBalance] = useState<number>();
-  const router = useRouter();
+  const [transactions, setTransactions] = useState<TransactionConfig[]>([])
+  const [balance, setBalance] = useState<number>()
+  const router = useRouter()
 
   useEffect(() => {
-    const allTransactions = getAllTransactions();
-    setTransactions(allTransactions);
+    const allTransactions = getAllTransactions()
+    setTransactions(allTransactions)
 
-    const balanceStats = getBalanceStatus();
+    const balanceStats = getBalanceStatus()
     if (balanceStats) {
-      const currentBalanceAmount = getCurrentBalanceAmount(allTransactions, balanceStats);
-      setBalance(currentBalanceAmount);
+      const currentBalanceAmount = getCurrentBalanceAmount(
+        allTransactions,
+        balanceStats
+      )
+      setBalance(currentBalanceAmount)
     }
-  }, []);
-  
+  }, [])
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
@@ -44,26 +51,38 @@ function List() {
             </tr>
           </thead>
           <tbody>
-            {transactions.map(transaction => {
-              return (
-                <tr key={transaction.id} className={styles.tableRow} onClick={() => router.push(`/transactions/${transaction.id}`)}>
-                  <td>{transaction.type}</td>
-                  <td>{transaction.amount}</td>
-                  <td>{format(transaction.date, 'dd/MM/yyyy')}</td>
-                  <td>{transaction.interval && `every ${transaction.interval.amount} ${transaction.interval.timePeriod}`}</td>
-                </tr>
-              )
-            })}
+            {transactions.map((transaction) => (
+              <tr
+                key={transaction.id}
+                className={styles.tableRow}
+                onClick={() => router.push(`/transactions/${transaction.id}`)}
+              >
+                <td>{transaction.type}</td>
+                <td>{transaction.amount}</td>
+                <td>{format(transaction.date, 'dd/MM/yyyy')}</td>
+                <td>
+                  {transaction.interval &&
+                    `every ${transaction.interval.amount} ${transaction.interval.timePeriod}`}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <div className={styles.balancePanel}>
-          <span>
-            Current Balance: 
-          </span>
-          <input value={balance || ''} type="number" onChange={(e) => setBalance(Number(e.target.value))}/>
-          <button disabled={!balance} onClick={() => {
-            createOrUpdateBalanceStatus(balance!);
-          }}>Update Balance</button>
+          <span>Current Balance:</span>
+          <input
+            value={balance || ''}
+            type="number"
+            onChange={(e) => setBalance(Number(e.target.value))}
+          />
+          <button
+            disabled={!balance}
+            onClick={() => {
+              createOrUpdateBalanceStatus(balance!)
+            }}
+          >
+            Update Balance
+          </button>
         </div>
       </div>
     </div>
