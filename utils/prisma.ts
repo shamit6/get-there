75d095c6c31ac945d21a6acdf956560ff93445d4
shamit6 <amitsh@wix.com>
2021-07-24 17:58:19 +0300

    add next auth (#3)

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from '@prisma/client'
-export type { BalanceStatus } from '@prisma/client'
+export type { BalanceStatus, User } from '@prisma/client'
 
 export const prismaClient = new PrismaClient()
