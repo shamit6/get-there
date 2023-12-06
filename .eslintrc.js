3ced085ccc4af1316a4acc9b293ee54d51371bdc
shamit6 <amishtalev@gmail.com>
2023-12-06 11:43:20 +0200

    upgrade deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,20 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
+  extends: ['next', 'next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        "next",
-        "next/core-web-vitals",
-        'prettier'
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: "module",
-    },
-    plugins: [
-        "react",
-        "@typescript-eslint",
-        "unused-imports",
-        "prettier",
-    ],
-    rules: {
-        "prettier/prettier": "warn",
-		"unused-imports/no-unused-imports": "error",
-		// "unused-imports/no-unused-vars": [
-		// 	"warn",
-		// 	{ "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
-		// ]
-    }
-};
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'unused-imports', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'unused-imports/no-unused-imports': 'error',
+    // "unused-imports/no-unused-vars": [
+    // 	"warn",
+    // 	{ "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+    // ]
+  },
+}
